d850fa0bfe1d8629648c4c484035e05cab1e85d0
Amanda Gettys <afgettys@gmail.com>
2020-05-28 15:27:00 -0400

    adding questions

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require('fs');
+
+const questions =[
+{
+    type: "input",
+    name: "title",
+    message: "Please give a title to this application"
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Please inform Github user name:"
+  },
+  {
+    type: "input",
+    name:"description",
+    message: "Please give a brief description of the application:"    
+  }, 
+  {
+    type: "input",
+    name:"instalation",
+    message: "Please provide instructions of instalation:"    
+  }, 
+  {
+    type: "input",
+    name:"usage",
+    message: "How is the usage of this application?"    
+  },  
+  {
+    type: "input",
+    name:"usage",
+    message: "How is the usage of this application?"    
+  }, 
+];
+
+//).then(data) => {
+
+  var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+
+  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success!");
+
+  });
+};
+
+
+
+
+
+
+
+
+ [
 
 ];
 
