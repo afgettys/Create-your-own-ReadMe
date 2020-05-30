3bef1e35dc346be5b61cb838df279aff434cb7b6
Amanda Gettys <afgettys@gmail.com>
2020-05-30 16:40:15 -0400

    got inquirer working

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
-const badge = generateMarkdown
+const badge = require('./generateMarkdown');
 
 const questions =[
 {
 type: "input",
 name: "title",
-message: "Please give a title to this application"
+message: "Please give a title to this application:"
 },
 {
 type: "input",
@@ -31,56 +31,51 @@ message: "How is the usage of this application?"
 {
 type: "input",
 name:"license",
-message:"What license you like to use for this application?Answer none if none"
+message:"License used for this application?Answer none if none"
 },
 {
 type: "input",
 name:"contributors",
-message: "Where there other contributors for this application?"    
+message: "Contributors for this application:"    
 }, 
 ];
 
 function init() {
 inquirer.prompt(questions).then((answer) => {
-fs.appendFileSync("README.md",("#" + answer.title)+ '\n',function(err) {
-if (err) {
+
+fs.appendFileSync("README.md", ("#" + answer.title + badge)+ 
+'\n',function(err) {
+if (err){
 return console.log(err);
 }
-console.log("Success!");    
+else {console.log("Success!")};    
   }
-)});
+)},
 
+fs.appendFileSync("README.md", ("This application was developed by: " + answer.userName + 
+'\n') + '\n', function(err) { 
+if (err) { 
+console.log(err);
+}
+else {console.log("Success")}})
 
-
-//The join() method returns the array as a string.
-
-//The elements will be separated by a specified separator. The default separator is comma (,).
-//given
-//fs.writeFile(filename, JSON.stringify(data, null, '\t'), 
-
-//The split() method separates an original string into an array of substrings, based on a separator string that you pass as input. The original string is not altered by split().
-//given
-//var filename = data.name.toLowerCase().split(' ').join('') + ".json";
-
-//example
-
- // Turn the arrays into JSON strings so they can be written to files
- //const dogJSON = JSON.stringify(dogs, null, 2);
- //const catJSON = JSON.stringify(cats, null, 2);
-
- //fs.writeFile("dogs.json", dogJSON, function(err) {
-   //if (err) {
-     //throw err;
-  // }
-
-  // console.log("Successfully wrote to dogs.json file");
-// });
-
+)};
 
 
 //given
 //function writeToFile(fileName, data) {}
 
+// \t Only one section at a time is visible.
+//The join() method returns the array as a string.
+//The elements will be separated by a specified separator. The default separator is comma (,).
+//The split() method separates an original string into an array of substrings, based on a separator string that you pass as input. The original string is not altered by split().
+//fs.writeFile(filename, JSON.stringify(data, null, '\t'), 
+//var filename = data.name.toLowerCase().split(' ').join('') + ".json";
 
 
-init();}
+
+
+
+
+init()
+
