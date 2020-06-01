d2722d79d7b30118525300622491b7de77d016f9
Amanda Gettys <afgettys@gmail.com>
2020-06-01 17:28:18 -0400

    uploading screnshots

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -7,24 +7,31 @@ function generateMarkdown(data) {
   `https://github.com/${data.title}/${data.projectName}/${data.userName}/${data.description}/${data.instalation}/${data.usage}/${data.license}/${data.contributors}`)
 }\n
 
-#**TITLE**:|${data.title} 
------------|-------------------
-##*PROJECT NAME*:|${data.projectName}
------------|-------------------
-##USER NAME:|${data.userName}
-------------|------------------
-##DESCRIPTION OF THE PROJECT:|${data.description}
------------------------------|---------------------
-##INSTALATION INSTRUCTIONS: |${data.instalation}
-----------------------------|---------------------
-##USAGE:|${data.usage}
---------|----------------------
-##LICENSE:|${data.license}
-----------|--------------------
-##CONTRIBUTORS:| ${data.contributors}
----------------|--------------------
+#**TITLE**:
+${data.title} 
 
-![Screenshots.](/image/screenshot.jpg)
+##*PROJECT NAME*:
+${data.projectName}
+
+##USER NAME:
+${data.userName}
+
+##DESCRIPTION OF THE PROJECT:
+${data.description}
+
+##INSTALATION INSTRUCTIONS: 
+${data.instalation}
+
+##USAGE:
+${data.usage}
+
+##LICENSE:
+${data.license}
+
+##CONTRIBUTORS:
+${data.contributors}
+
+![Screenshots.](image/screenshot.jpg)
 `
 }
 
